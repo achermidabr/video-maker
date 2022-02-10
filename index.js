52c9dcfb5048b3d08148a9d0462027dae8e9d99f
Alexandre Hermida <alexandrehermida@ima.sc.gov.br>
2022-02-10 09:41:02 -0300

    activity 2: perguntar pelo prefixo

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const readline = require('readline-sync')
 
 function start(){
+    const prefixes = ['Who is','What is','The history of']
     const content = {}
+    
     content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
 
     function askAndReturnSearchTerm(){
-        
         return readline.question('Type a Wikipedia search term: ')
+    }
+
+    function askAndReturnPrefix(){
+        const selectedPrefixIndex = readline.keyInSelect(prefixes)
+
+        return prefixes[selectedPrefixIndex]
     }
 
     console.log(content)
